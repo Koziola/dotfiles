fdac5ba6f47db3b11372cec22f6976b704573f3d
Adam Koziol <adamkoz@stripe.com>
2022-10-20 10:39:38 -0600

    vim: cleaning up lsp settings, adding autofmt

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -4,34 +4,52 @@ vim.lsp.set_log_level('info')
 lspconfig = require 'lspconfig'
 completion = require 'cmp_nvim_lsp'
 lsp_status = require 'lsp-status'
-require('trouble').setup()
 
-local completion_capabilities = completion.update_capabilities(vim.lsp.protocol.make_client_capabilities())
+--coq = require 'coq'
+
+--require('trouble').setup()
+local completion_capabilities = completion.default_capabilities()
 
 local custom_capabilities = {}
 table.insert(custom_capabilities, lsp_status.capabilities)
 table.insert(custom_capabilities, completion_capabilities)
 
 local custom_on_attach = function(client)
--- completion.on_attach(client)
 lsp_status.on_attach(client)
 end
 
 lspconfig.gopls.setup {
-on_attach=custom_on_attach,
-capabilities=custom_capabilities,
-cmd = {'gopls', 'serve'},
-settings = {
-  gopls = {
-    analyses = {
-      unusedparams = true,
+  on_attach=custom_on_attach,
+  capabilities=custom_capabilities,
+  cmd = {'gopls', 'serve'},
+  settings = {
+    gopls = {
+      analyses = {
+        unusedparams = true,
+      },
+      staticcheck = true,
     },
-    staticcheck = true,
   },
-},
 }
 
 lspconfig.tsserver.setup {
-on_attach=custom_on_attach,
-capabilities=custom_capabilities
+  on_attach=custom_on_attach,
+  capabilities=custom_capabilities,
+  cmd = {
+    'typescript-language-server',
+    '--stdio',
+    '--tsserver-path=' .. vim.fn.getenv("HOME") .. '/.nodenv/shims/tsserver',
+  },
+  cmd_env = { NODE_OPTIONS = "--max-old-space-size=8192" }, -- give 8gb of RAM to node
+  init_options = {
+    maxTsServerMemory = "8192",
+    preferences = {
+      -- always import absolute paths
+      importModuleSpecifierPreference = "non-relative",
+    }
+  }
 }
+
+vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]]
+
+
