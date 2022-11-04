3f8fce74d79b84f6c2622223cd92502ccfc29fb4
Adam Koziol <adamkoz@stripe.com>
2022-11-04 10:59:33 -0600

    vim: try using lsp-zero
    Avoids the need to manually configure and install a bunch of other
    plugins?  Seems to work pretty well so far.

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,60 +1,60 @@
-vim.g.completion_matching_strategy_list = {'exact', 'substring', 'fuzzy'}
-vim.lsp.set_log_level('info')
+--vim.g.completion_matching_strategy_list = {'exact', 'substring', 'fuzzy'}
+--vim.lsp.set_log_level('info')
 
-lspconfig = require 'lspconfig'
-completion = require 'cmp_nvim_lsp'
-lsp_status = require 'lsp-status'
+--lspconfig = require 'lspconfig'
+--completion = require 'cmp_nvim_lsp'
+--lsp_status = require 'lsp-status'
 
---coq = require 'coq'
+----coq = require 'coq'
 
---require('trouble').setup()
-local completion_capabilities = completion.default_capabilities()
+----require('trouble').setup()
+--local completion_capabilities = completion.default_capabilities()
 
-local custom_capabilities = {}
-table.insert(custom_capabilities, lsp_status.capabilities)
-table.insert(custom_capabilities, completion_capabilities)
+--local custom_capabilities = {}
+--table.insert(custom_capabilities, lsp_status.capabilities)
+--table.insert(custom_capabilities, completion_capabilities)
 
-local custom_on_attach = function(client)
-  lsp_status.on_attach(client)
-  client.server_capabilities.document_formatting = false
-  client.server_capabilities.document_range_formatting = false
+--local custom_on_attach = function(client)
+  --lsp_status.on_attach(client)
+  --client.server_capabilities.document_formatting = false
+  --client.server_capabilities.document_range_formatting = false
 
-  if client.config.flags then
-    client.config.flags.allow_incremental_sync = true
-  end
-end
+  --if client.config.flags then
+    --client.config.flags.allow_incremental_sync = true
+  --end
+--end
 
-lspconfig.gopls.setup {
-  on_attach=custom_on_attach,
-  capabilities=custom_capabilities,
-  cmd = {'gopls', 'serve'},
-  settings = {
-    gopls = {
-      analyses = {
-        unusedparams = true,
-      },
-      staticcheck = true,
-    },
-  },
-}
+--lspconfig.gopls.setup {
+  --on_attach=custom_on_attach,
+  --capabilities=custom_capabilities,
+  --cmd = {'gopls', 'serve'},
+  --settings = {
+    --gopls = {
+      --analyses = {
+        --unusedparams = true,
+      --},
+      --staticcheck = true,
+    --},
+  --},
+--}
 
-lspconfig.tsserver.setup {
-  on_attach=custom_on_attach,
-  capabilities=custom_capabilities,
-  cmd = {
-    'typescript-language-server',
-    '--stdio',
-    '--tsserver-path=' .. vim.fn.getenv("HOME") .. '/.nodenv/shims/tsserver',
-  },
-  cmd_env = { NODE_OPTIONS = "--max-old-space-size=8192" }, -- give 8gb of RAM to node
-  init_options = {
-    maxTsServerMemory = "8192",
-    preferences = {
-      -- always import absolute paths
-      importModuleSpecifierPreference = "non-relative",
-    }
-  }
-}
+--lspconfig.tsserver.setup {
+  --on_attach=custom_on_attach,
+  --capabilities=custom_capabilities,
+  --cmd = {
+    --'typescript-language-server',
+    --'--stdio',
+    --'--tsserver-path=' .. vim.fn.getenv("HOME") .. '/.nodenv/shims/tsserver',
+  --},
+  --cmd_env = { NODE_OPTIONS = "--max-old-space-size=8192" }, -- give 8gb of RAM to node
+  --init_options = {
+    --maxTsServerMemory = "8192",
+    --preferences = {
+      ---- always import absolute paths
+      --importModuleSpecifierPreference = "non-relative",
+    --}
+  --}
+--}
 
 vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]]
 
--- nvim/lua/deps.lua
+++ nvim/lua/deps.lua
@@ -55,8 +55,30 @@ return require('packer').startup(function(use)
   use 'hrsh7th/cmp-path'
   --use 'hrsh7th/cmp-buffer'
   
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    requires = {
+    -- LSP Support
+      {'neovim/nvim-lspconfig'},
+      {'williamboman/mason.nvim'},
+      {'williamboman/mason-lspconfig.nvim'},
+
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-buffer'},
+      {'hrsh7th/cmp-path'},
+      {'saadparwaiz1/cmp_luasnip'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'hrsh7th/cmp-nvim-lua'},
+
+      -- Snippets
+      {'L3MON4D3/LuaSnip'},
+      {'rafamadriz/friendly-snippets'},
+    }
+  }
+  
   --use 'ms-jpq/coq_nvim' -- , {'branch':'coq'}
-  use 'folke/trouble.nvim'
+  --use 'folke/trouble.nvim'
   use 'folke/lua-dev.nvim'
   
   -- Floating terminal windows
