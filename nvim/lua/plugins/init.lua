c0028ece395ce9b1d8e52e1ef064e1ee021e3380
Adam Koziol <adamkoz@stripe.com>
2023-10-20 06:54:10 -0600

    vim: disable additional regex highlighting
    Hopefully also boosts performance

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -228,7 +228,10 @@ return {
       require('nvim-treesitter.configs').setup({
         -- Add languages here that you want installed for treesitter
         ensure_installed = { 'lua', 'typescript', 'tsx', 'java', 'go', 'ruby', 'bash', 'markdown' },
-        highlight = { enable = true },
+        highlight = { 
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
         indent = {
           enable = true,
           -- Treesitter indentation for lua files has problems
