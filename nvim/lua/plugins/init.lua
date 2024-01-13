34cae28345557b5b58bb8e4ad275a928d02bf116
Adam Koziol <adam.j.koziol@gmail.com>
2024-01-13 14:15:53 -0700

    vim: add harpoon

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -403,5 +403,24 @@ return {
         group = format_group,
       })
     end
+  },
+  { 
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local harpoon = require("harpoon")
+
+      harpoon:setup()
+      -- Need to figure out some better keybinds here
+      vim.keymap.set('n', "<C-'>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+      vim.keymap.set('n', "<leader>H", function() harpoon:list():append() end)
+      vim.keymap.set('n', "<leader>ha", function() harpoon:list():select(1) end)
+      vim.keymap.set('n', "<leader>hs", function() harpoon:list():select(2) end)
+      vim.keymap.set('n', "<leader>hd", function() harpoon:list():select(3) end)
+      vim.keymap.set('n', "<leader>hf", function() harpoon:list():select(4) end)
+      vim.keymap.set("n", "<leader>hn", function() harpoon:list():next() end)
+      vim.keymap.set("n", "<leader>hp", function() harpoon:list():prev() end)
+    end
   }
 }
