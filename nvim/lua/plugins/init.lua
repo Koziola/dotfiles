b579b31d2f4d0064157585d1a21791fb4e55e072
Adam Koziol <adamkoz@stripe.com>
2025-01-07 09:03:56 -0700

    vim: update plugins, update deprecated var

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -106,7 +106,7 @@ return {
       -- This is required to support older version of fzf on remote devboxes
       fzf_opts = { ['--border'] = false },
       -- These settings reduce lag from slow git operations
-      global_git_icons = false,
+      git_icons = false,
       git = {
         files = {
           previewer = false,
