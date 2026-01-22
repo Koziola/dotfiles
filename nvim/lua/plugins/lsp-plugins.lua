4b21f6197c1b409158fac71f88db2cca0ee43754
Adam Koziol <adamkoz@stripe.com>
2026-01-22 12:32:14 -0700

    vim: rm yamlfmt

--- nvim/lua/plugins/lsp-plugins.lua
+++ nvim/lua/plugins/lsp-plugins.lua
@@ -203,7 +203,6 @@ return {
             java = { "zoolander_format_java" },
             scala = { "zoolander_format_scala" },
             terraform = { "sc_terraform" },
-            yaml = { "yamlfmt" },
           },
         formatters = {
           goimports = {
